99f4d5e9ed364add1705a8fa3b6384173e870d4e
hannes <hannes.dummy@putz-transporte.at>
2017-04-24 00:50:41 +0200

    display speed on alphanum display works

--- megatrak_sensor_ctrl.ino
+++ megatrak_sensor_ctrl.ino
@@ -3,6 +3,9 @@
 #include <RH_RF69.h>
 #include <EEPROM.h>
 #include <SerialCommand.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include "Adafruit_LEDBackpack.h"
 
 #define RESTART_ADDR       0xE000ED0C
 #define READ_RESTART()     (*(volatile uint32_t *)RESTART_ADDR)
@@ -13,7 +16,28 @@
 uint8_t SERVER_ADDRESS = EEPROM.read(EEPROM_SERVER_ADDRESS);
 uint8_t NODE_ADDRESS = EEPROM.read(EEPROM_NODE_ADDRESS);
 
+#define EEPROM_Display_en_ADDRESS 15
+
 SerialCommand cmd;
+
+Adafruit_AlphaNum4 lcd1 = Adafruit_AlphaNum4();
+Adafruit_AlphaNum4 lcd2 = Adafruit_AlphaNum4();
+
+typedef union {
+	struct {
+		unsigned char bit1 : 1;
+		unsigned char bit2 : 1;
+		unsigned char bit3 : 1;
+		unsigned char bit4 : 1;
+		unsigned char bit5 : 1;
+		unsigned char bit6 : 1;
+		unsigned char bit7 : 1;
+		unsigned char bit8 : 1;
+	}bits;
+	unsigned char displays;
+}DisplayEn;
+
+DisplayEn dis;
 
 RH_RF69 rf69(15, 14);
 RHDatagram manager(rf69, NODE_ADDRESS);
@@ -36,11 +60,14 @@ void setup() {
 	Serial.print("rfm69 server startup nr: ");
 	Serial.println(SERVER_ADDRESS);
 
+	dis.displays=EEPROM.read(EEPROM_Display_en_ADDRESS);
+
 	//###################################################
 	cmd.addCommand("sna", sc_setNodeAddress); //set node address
 	cmd.addCommand("ssa", sc_setServerAddress); //set server address
 	cmd.addCommand("cfg", sc_printCfg);
 	cmd.addCommand("scn", sc_sndCmdNode); //send a command to a node over rf
+	cmd.addCommand("sde", sc_setDisplay);
 	cmd.setDefaultHandler(sc_default);
 	//###################################################
 
@@ -54,13 +81,28 @@ void setup() {
 	// If you are using a high power RF69, you *must* set a Tx power in the
 	// range 14 to 20 like this:
 	rf69.setTxPower(14);
+
+	//if (dis.bits.bit1) {
+		lcd1.begin(0x70);
+		lcd1.clear();
+		delay(200);
+		lcd1.setBrightness(3);
+		lcd1.writeDisplay();
+	//}
+
+	//if (dis.bits.bit2) {
+		lcd2.begin(0x71);
+		lcd2.clear();
+		delay(200);
+		lcd2.setBrightness(3);
+		lcd2.writeDisplay();
+	//}
+
 }
 
 void loop() {
 	cmd.readSerial();
 	chkMsg();
-	//involtReceive();
-	//involt_fname = "";
 }
 
 void chkMsg() {
@@ -106,7 +148,7 @@ void chkMsg() {
 		else {
 			Serial.println("recv failed");
 		}
-		//memset(msg, 0, sizeof(msg)); //clearing the msg array IMPORTANT!!!!! otherwise old values from previous messages stay in there!!!
+		memset(msg, 0, sizeof(msg)); //clearing the msg array IMPORTANT!!!!! otherwise old values from previous messages stay in there!!!
 
 	}
 }
@@ -128,7 +170,26 @@ void decodeSpeed(uint8_t* msg, uint8_t from) {
 	speed.val.b[2] = msg[4];
 	speed.val.b[3] = msg[5];
 
-	printSpeed();
+	if (dis.bits.bit1) {
+		//temporarily holds data from vals
+		char charVal[6];
+
+		//4 is mininum width, 3 is precision; float value is copied onto buff
+		dtostrf(speed.val.f, 5, 2, charVal);
+		//Serial.println(charVal);
+		ledprint(charVal, &lcd1);
+	}
+	if (dis.bits.bit2) {
+		//temporarily holds data from vals
+		char charVal[6];
+
+		//4 is mininum width, 3 is precision; float value is copied onto buff
+		dtostrf(speed.val.f, 5, 2, charVal);
+		//Serial.println(charVal);
+		ledprint(charVal, &lcd2);
+	}
+
+	//printSpeed();
 }
 
 void printSpeed() {
@@ -151,6 +212,44 @@ char* translateType(SPEED s) {
 	}
 }
 
+void ledprint(char* str, Adafruit_AlphaNum4 *alphanum)
+{
+	uint8_t j = 0;
+	for (int i = 0; i < strlen(str); i++) {
+		if (str[i+1] == '.') {
+			alphanum->writeDigitAscii(j, str[i],true);
+			j++;
+		}
+		else {
+			if (str[i] != '.') {
+				alphanum->writeDigitAscii(j, str[i]);
+				j++;
+			}
+		}
+	}
+	//lcd1.writeDigitAscii(0, str[0]);
+	//lcd1.writeDigitAscii(1, str[1]);
+	//lcd1.writeDigitAscii(2, str[2]);
+	//lcd1.writeDigitAscii(3, str[3]);
+
+	//for (int i = 0; i < strlen(str); i++)      //for each character in str
+	//{
+	//	//if ('.' == str[i+1])                    //if the next character after this one is '.'
+	//	//{
+	//	//	alphanum->writeDigitAscii(i, str[i], true);  //write the '.' along with the character
+	//	//	i++;                                  //skip over the '.'
+	//	//}
+	//	//else
+	//	//{
+	//	Serial.print(str[i]);
+	//	Serial.print(".");
+	//		alphanum->writeDigitAscii(i, str[i]);  //write the character
+	//	//}
+	//}
+	//Serial.println("");
+	alphanum->writeDisplay();  //write to the display.
+	alphanum->clear();
+}
 
 //###################################################
 void sc_sndCmdNode() {
@@ -240,6 +339,23 @@ void sc_printCfg() {
 	Serial.println(getEEPROMNodeAddress());
 	Serial.print("server address: ");
 	Serial.println(getEEPROMServerAddress());
+
+	Serial.print("display: ");
+	Serial.print(dis.bits.bit8);
+	Serial.print("|");
+	Serial.print(dis.bits.bit7);
+	Serial.print("|");
+	Serial.print(dis.bits.bit6);
+	Serial.print("|");
+	Serial.print(dis.bits.bit5);
+	Serial.print("|");
+	Serial.print(dis.bits.bit4);
+	Serial.print("|");
+	Serial.print(dis.bits.bit3);
+	Serial.print("|");
+	Serial.print(dis.bits.bit2);
+	Serial.print("|");
+	Serial.println(dis.bits.bit1);
 }
 
 void sc_default(const char *command) {
@@ -247,7 +363,64 @@ void sc_default(const char *command) {
 	Serial.println("ssa ... set server adress");
 	Serial.println("sna ... set this nodes adress");
 	Serial.println("scn ... send command to node");
-
-
+	Serial.println("sde - en|disable display: sde [1|2] [0|1]");
 }
 
+void sc_setDisplay() {
+	char *arg;
+	uint16_t dNr;
+	uint16_t val;
+	arg = cmd.next();
+	if (arg != NULL) {
+		dNr = atoi(arg);    // Converts a char string to an integer
+	}
+	else {
+		Serial.println("sre - revNr is missing! aborting...");
+		return;
+	}
+	arg = cmd.next();
+	if (arg != NULL) {
+		val = atoi(arg);    // Converts a char string to an integer
+	}
+	else {
+		Serial.println("sre - val is missing! aborting...");
+		return;
+	}
+	setEEPROMDisplayEnable(dNr, val);
+}
+
+void setEEPROMDisplayEnable(uint8_t disNr, bool val) {
+	Serial.println("setting display ");
+	Serial.print(disNr);
+	Serial.print(" to ");
+	Serial.println(val);
+
+	switch (disNr) {
+	case 1:
+		dis.bits.bit1 = val;
+		break;
+	case 2:
+		dis.bits.bit2 = val;
+		break;
+	case 3:
+		dis.bits.bit3 = val;
+		break;
+	case 4:
+		dis.bits.bit4 = val;
+		break;
+	case 5:
+		dis.bits.bit5 = val;
+		break;
+	case 6:
+		dis.bits.bit6 = val;
+		break;
+	case 7:
+		dis.bits.bit7 = val;
+		break;
+	case 8:
+		dis.bits.bit8 = val;
+		break;
+	}
+	EEPROM.write(EEPROM_Display_en_ADDRESS, dis.displays);
+	Serial.println("...done");
+}
